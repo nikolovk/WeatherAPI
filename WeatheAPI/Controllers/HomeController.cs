b459ce3d91876904a56746cbe5334f91d6084b11
Krasimir Nikolov <krasi.nikolov@gmail.com>
2013-05-15 20:23:42 +0300

    start

---
+++ WeatheAPI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeatheAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetWeather(string latitude, string longitude)
+        {
+            string today = DateTime.Now.ToString("yyyy-MM-dd");
+            string url = "http://api.worldweatheronline.com/free/v1/weather.ashx?";
+            string data = "key=5phbpfubtbrbc3usc9kg2fw3&q=" + latitude + "," + longitude +
+                "&cc=no&date=" + today + "&format=json";
+            WebRequest webRequest = WebRequest.Create(url+data);
+            WebResponse response = webRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            return Json(responseFromServer, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
